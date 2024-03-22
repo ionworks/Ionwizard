37637e2565125b919bb02ce0229c2abd971659c7
kratman <kratmandu@gmail.com>
2024-03-22 14:13:46 -0400

    Download into a temp folder

--- ionwizard/container_wizard.py
+++ ionwizard/container_wizard.py
@@ -1,6 +1,8 @@
+import os
 import sys
 import subprocess
 import yaml
+from tempfile import TemporaryDirectory
 
 
 class IonWorksImageWizard:
@@ -15,15 +17,19 @@ class IonWorksImageWizard:
         return head + version + "/" + tail
 
     @staticmethod
-    def fetch_image(lib_name, web_address, key):
-        err = subprocess.call(["curl", "-sSLO", "-L", web_address, "-u", key])
+    def fetch_image(lib_name, web_address, key, location):
+        err = subprocess.call(
+            ["curl", "-sSLO", "--output-dir", location, "-L", web_address, "-u", key]
+        )
         if err != 0:
             raise RuntimeError(f"\nInstallation failed for {lib_name}.\n")
 
     @staticmethod
-    def load_image(product):
+    def load_image(product, location):
         zip_name = IonWorksImageWizard.get_zip_name(product)
-        err = subprocess.call([f"docker load < {zip_name}"], shell=True)
+        err = subprocess.call(
+            [f"docker load < {os.path.join(location, zip_name)}"], shell=True
+        )
         if err != 0:
             raise RuntimeError(f"\nDocker loading failed for {product}.\n")
 
@@ -52,14 +58,19 @@ class IonWorksImageWizard:
             raise ValueError(
                 "Invalid configuration file. Only 1 docker image can be specified."
             )
-        addr = IonWorksImageWizard.get_address(config["version"], config["product"])
-        IonWorksImageWizard.fetch_image(
-            config["product"], addr, f"license:{config['key']}"
-        )
-        IonWorksImageWizard.load_image(config["product"])
+        IonWorksImageWizard.make_container(config)
         IonWorksImageWizard.run_image(
             config["product"], config["key"], config["version"]
         )
+
+    @staticmethod
+    def make_container(config):
+        with TemporaryDirectory() as image_dir:
+            addr = IonWorksImageWizard.get_address(config["version"], config["product"])
+            IonWorksImageWizard.fetch_image(
+                config["product"], addr, f"license:{config['key']}", image_dir
+            )
+            IonWorksImageWizard.load_image(config["product"], image_dir)
 
     @staticmethod
     def process_config(file_name):
