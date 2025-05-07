1b03ef43d118fca62c085a02e691c26c6f9aa43c
kratman <kratmandu@gmail.com>
2025-05-07 09:53:54 -0400

    Style and optimization

--- ionwizard/notifications.py
+++ ionwizard/notifications.py
@@ -3,10 +3,14 @@ import subprocess
 from ionwizard.library_wizard import IonWorksPipWizard
 
 
+def get_public_ionworks_package_names() -> list[str]:
+    return ["ionwizard", "iwutil"]
+
+
 def find_outdated(library_config):
     outdated_found = False
     licenses, package_names = get_packages_and_keys(library_config)
-    cleaned_output = ["\nThere are newer versions of these ionworks packages available:\n"]
+    cleaned_output = ["\nNewer versions of these ionworks packages available:\n"]
     for key in licenses:
         output = get_outdated_from_pip(key)
         for line in output:
@@ -37,7 +41,6 @@ def get_outdated_from_pip(key):
 
 def get_packages_and_keys(library_config):
     lib_info = {lib["library"]: lib["key"] for lib in library_config}
-    lib_info.update({"ionwizard": None})
-    package_names = list(lib_info.keys())
-    licenses = list(lib_info.values())
-    return licenses, package_names
+    package_names = list(lib_info.keys()) + get_public_ionworks_package_names()
+    licenses = list(lib_info.values()) + [None]
+    return set(licenses), set(package_names)
